ff230c7a018a8ff548315318e28c0af1c1cb4c7c
matt pero <mattpero22@gmail.com>
2022-09-27 18:43:17 -0400

    commit before branch testing npm particle package

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+//react and npm packages
 import React from 'react';
+import { tsParticles } from 'tsparticles-engine';
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import { useCallback } from "react";
+
+//styles
 import './App.css';
+//components
 import Header from './components/Header'
 
-function App() {
+const App = () => {
   return (
     <div id='app'>
       <Header> </Header>
