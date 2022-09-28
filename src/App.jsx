91790196af5b8e84d3fd5565a0e63c80b9423a51
matt pero <mattpero22@gmail.com>
2022-09-28 09:18:32 -0400

    changed bg color for dark, commit to test particle effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 //react and npm packages
 import React from 'react';
-import { tsParticles } from 'tsparticles-engine';
-import Particles from "react-particles";
-import { loadFull } from "tsparticles";
-import { useCallback } from "react";
 
 //styles
 import './App.css';
+
 //components
 import Header from './components/Header'
 
 const App = () => {
   return (
-    <div id='app'>
+    <div className="App"> 
       <Header> </Header>
     </div>
-  );
-}
+  )
+};
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ const Header = () => {
         <div id='header'>
             <h1 id='name'>MATT PERO</h1>
             <div id='header-tabs'>
-                <a id='about' className='tab'>ABOUT</a>
-                <a id='projects' className='tab'>PROJECTS</a>
-                <a id='skills' className='tab'>SKILLS</a>
-                <a id='education' className='tab'>EDUCATION</a>
+                <div id='about' className='tab'>ABOUT</div>
+                <div id='projects' className='tab'>PROJECTS</div>
+                <div id='skills' className='tab'>SKILLS</div>
+                <div id='education' className='tab'>EDUCATION</div>
             </div>
         </div>
     );
