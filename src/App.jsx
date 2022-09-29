4cf72c486e9ca032fa17d815df01efa7dc877873
matt pero <mattpero22@gmail.com>
2022-09-29 09:31:41 -0400

    refactored theme state by passing it as prop from parent component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-//react and npm packages
+//react hooks and npm packages
 import React from 'react';
+import { useState } from "react";
 
 //styles
 import './App.css';
@@ -10,11 +11,14 @@ import Particles from './components/Particles';
 import ThemeToggle from './components/ThemeToggle';
 
 const App = () => {
+
+  const [theme, setTheme] = useState("dark")
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <Particles> </Particles>
       <Header> </Header>
-      <ThemeToggle> </ThemeToggle>
+      <ThemeToggle theme={theme} setTheme={setTheme}> </ThemeToggle>
     </div>
   )
 };
--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
-
-const ThemeToggle = () => {
-
-    const [ theme , setTheme ] = useState("dark")
-
+const ThemeToggle = (props) => {
     const handleTheme = (event) => {
-        event.target.className === "dark" ? setTheme("light") : setTheme("dark")
+        event.target.className === "dark" ? props.setTheme("light") : props.setTheme("dark")
+        console.log(props.theme)
     }
-
     return (
-        <h1 className={theme} onClick={handleTheme}>{theme}</h1>
+        <button className={props.theme} onClick={handleTheme}> Change Theme </button>
     )
 }
 
