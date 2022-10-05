881de5f5cc8ca6463a9ee39fdaeb4744d568b93c
matt pero <mattpero22@gmail.com>
2022-10-05 13:37:34 -0400

    created icons for all socials links via a static img

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -2,11 +2,17 @@ const Socials = (props) => {
     return (
         <div id="socials">
             <div id="github" className="tab">
-                <img className="social_icon" src={`./img/github-logo-${props.theme}-theme.png`}></img>
+                <img className="social_icon" src={`./img/github-logo-${props.theme}-theme.png`} alt='github'></img>
             </div>
-            <div id="email" className="tab">linkedin</div>
-            <div id="resume" className="tab">resume</div>
-            <div id="email" className="tab">email</div>
+            <div id="linkedin" className="tab">
+                <img className="social_icon" src={`./img/linkedin-logo-${props.theme}-theme.png`} alt='linkedin'></img>
+            </div>
+            <div id="resume" className="tab">
+                <img className="social_icon" src={`./img/resume-logo-${props.theme}-theme.png`} alt='resume'></img>
+            </div>
+            <div id="email" className="tab">
+                <img className="social_icon" src={`./img/email-logo-${props.theme}-theme.png`} alt='email'></img>
+            </div>
         </div>
     );
 }
