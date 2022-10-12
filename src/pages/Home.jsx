9e54f1566de6035f36a71ffb2d2b22b677002753
matt pero <mattpero22@gmail.com>
2022-10-12 09:31:44 -0400

    styled the page selection tiles on the main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 const Home = (props) => {
     return (
         <div id="page_tiles">
-            <div className="tile"></div>
-            <div className="tile"></div>
-            <div className="tile"></div>
-            <div className="tile"></div>
+            <div className={`tile tile_${props.theme}`}>ABOUT</div>
+            <div className={`tile tile_${props.theme}`}>PROJECTS</div>
+            <div className={`tile tile_${props.theme}`}>SKILLS</div>
+            <div className={`tile tile_${props.theme}`}>EDUCATION</div>
         </div>
     )
 }
