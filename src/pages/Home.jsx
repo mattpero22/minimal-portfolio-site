8185eaa0aa5029938781b355fff9ab8f4ffa516e
matt pero <mattpero22@gmail.com>
2022-10-12 09:53:55 -0400

    added logos for all page tiles and styled

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 const Home = (props) => {
     return (
         <div id="page_tiles">
-            <div className={`tile tile_${props.theme}`}>ABOUT</div>
-            <div className={`tile tile_${props.theme}`}>PROJECTS</div>
-            <div className={`tile tile_${props.theme}`}>SKILLS</div>
-            <div className={`tile tile_${props.theme}`}>EDUCATION</div>
+            <div className={`tile tile_${props.theme}`}>
+                <img className="tile_logo" src={`./img/about-logo-${props.theme}-theme.png`} alt='about'></img>
+            </div>
+            <div className={`tile tile_${props.theme}`}>
+                <img className="tile_logo" src={`./img/projects-logo-${props.theme}-theme.png`} alt='about'></img>
+            </div>
+            <div className={`tile tile_${props.theme}`}>
+                <img className="tile_logo" src={`./img/skills-logo-${props.theme}-theme.png`} alt='about'></img>
+            </div>
+            <div className={`tile tile_${props.theme}`}>
+            <img className="tile_logo" src={`./img/edu-logo-${props.theme}-theme.png`} alt='about'></img>
+            </div>
         </div>
     )
 }
